4c67ba688db42572caad9e90a810c95fa24d509b
Donlaporn <dolaporn0906@gmail.com>
2020-07-20 15:14:40 +0700

    workshop_02

--- hello/src/app/app.component.ts
+++ hello/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title = 'DONLAPORN';
+  score = 100;
+  show = true;
+  firstName = '';
+  lastName ='';
+  name = '';
+  colors = [ 'Red', 'Green', 'Blue']; //Array
+
+
+  inputYourName(event: any): void{
+    this.firstName = event.target.value;
+  }
+
+  inputLastName(input: string): void{
+    this.lastName = input;
+  }
+
+  saveData(): void{
+    this.name = `${this.firstName} ${this.lastName}`;
+  }
 }
